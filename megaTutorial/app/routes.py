55e47fafcb1ab2c6ee1480efa7a5de7b1eadab17
BertoBraghequadre <gaetano.ippolito98@gmail.com>
2020-04-19 17:02:01 +0200

    Capitolo 13
    Completato

--- megaTutorial/app/routes.py
+++ megaTutorial/app/routes.py
@@ -1,6 +1,6 @@
 # Quì vengono gestiti tutti gli end-point, ovvero le loro root
 
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -8,6 +8,7 @@ from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app.email import send_password_reset_email
+from flask_babel import get_locale
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -92,6 +93,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locate = str(get_locale())
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
